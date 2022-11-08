24c658deb48e1a1333f716740626d5bf08a17b0e
Alexander Rind <1217527+alex-rind@users.noreply.github.com>
2022-11-08 11:28:53 +0100

    add D3 (v.7.6) & D3 types & scatterplot demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from "../components/TheWelcome.vue";
+import ScatterPlotDemo from "@/components/ScatterPlotDemo.vue";
 </script>
 
 <template>
   <main>
+    <ScatterPlotDemo />
     <TheWelcome />
   </main>
 </template>
