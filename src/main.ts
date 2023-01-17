54316b43de8335c31ad4b3e135fb843778b45f0b
Alexander Rind <1217527+alex-rind@users.noreply.github.com>
2023-01-17 14:56:58 +0100

    remove unneeded demo code from boilerplate

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import { store } from "./store";
 
-import "./assets/main.css"; // TODO: remove (part of vue hello world)
 import "@/assets/main.scss";
 
 const app = createApp(App);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
 import ScatterPlotDemo from "@/components/ScatterPlotDemo.vue";
 </script>
 
 <template>
   <main>
     <ScatterPlotDemo />
-    <TheWelcome />
   </main>
 </template>
