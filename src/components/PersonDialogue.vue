314a956ac6bb73b7b86b8f59741d7fea9d8f457f
Eliasboss2006 <elias.patru@gmail.com>
2024-08-09 14:20:44 +0200

    ADD mandatory field

--- src/components/PersonDialogue.vue
+++ src/components/PersonDialogue.vue
@@ -35,6 +35,7 @@
           <div class="control">
             <input
               class="input"
+              :class="{'is-danger': newPersonDetails.name.length < 1}"
               type="text"
               placeholder="Name"
               v-model="newPersonDetails.name"
@@ -132,7 +133,7 @@
       <button class="button is-white" @click="abort">
         {{ t("cancel") }}
       </button>
-      <button class="button is-link" @click="savePerson">
+      <button class="button is-link" @click="savePerson" :disabled="newPersonDetails.name.length < 1">
         {{ t("done") }}
       </button>
     </div>
