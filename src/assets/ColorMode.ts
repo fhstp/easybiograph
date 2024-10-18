24472c67bcb332b264ef607eeb7027b97d5b7d39
julia221501 <sakura.ju22150@gmail.com>
2024-10-18 10:39:36 +0200

    Rot statt grün bei Zeitpunkt

--- src/assets/ColorMode.ts
+++ src/assets/ColorMode.ts
@@ -1,11 +1,12 @@
 const colorModes = {
-    'green-mode': { primary: '#488193', secondary: '#d2dee2', text: 'white', secondaryText: 'black', moment: "#a1d592", button: "#333", buttonText: "white" },
+    'green-mode': { primary: '#488193', secondary: '#d2dee2', text: 'white', secondaryText: 'black', moment: "#c1121f", button: "#333", buttonText: "white" },
     'yellow-mode': { primary: '#deac18', secondary: '#F2DC99', text: 'black', secondaryText: 'black', moment: "#1252e6", button: "#333", buttonText: "white" },
     'black-mode': { primary: '#333333', secondary: '#666666', text: 'white', secondaryText: 'white', moment: "black",button: "#e3e3e3", buttonText: "black" }
   };
 
 //hell: F2BC1B, mittel: c99c16, dunkel: deac18
 //rot: c1121f, blau: 1240AB, neues blau: 1252e6
+//altes grün für Zeitpunkt: a1d592
 
 export function changeColorMode(newMode: string) {
   const colors = colorModes[newMode as 'green-mode' | 'yellow-mode' | 'black-mode'];
