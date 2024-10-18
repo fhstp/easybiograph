f187713cff751d62e4d35b4927cf72135f1b13c4
julia221501 <sakura.ju22150@gmail.com>
2024-10-18 09:12:03 +0200

    Gelb weniger dunkel

--- src/assets/ColorMode.ts
+++ src/assets/ColorMode.ts
@@ -1,8 +1,10 @@
 const colorModes = {
     'green-mode': { primary: '#488193', secondary: '#d2dee2', text: 'white', secondaryText: 'black', moment: "#a1d592", button: "#333", buttonText: "white" },
-    'yellow-mode': { primary: '#c99c16', secondary: '#F2DC99', text: 'black', secondaryText: 'black', moment: "#c1121f", button: "#333", buttonText: "white" },
+    'yellow-mode': { primary: '#deac18', secondary: '#F2DC99', text: 'black', secondaryText: 'black', moment: "#c1121f", button: "#333", buttonText: "white" },
     'black-mode': { primary: '#333333', secondary: '#666666', text: 'white', secondaryText: 'white', moment: "black",button: "#e3e3e3", buttonText: "black" }
   };
+
+//F2BC1B c99c16 deac18
 
 export function changeColorMode(newMode: string) {
   const colors = colorModes[newMode as 'green-mode' | 'yellow-mode' | 'black-mode'];
